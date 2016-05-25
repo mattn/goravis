a86a11d65d9401f21bada11535a79edecddb3620
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-25 23:40:46 +0900

    add sync

---
+++ sync.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/alecthomas/kingpin"
+)
+
+var syncCommand = kingpin.Command("sync", "triggers a new sync with GitHub").Action(func(ctx *kingpin.ParseContext) error {
+	err := auth()
+	if err != nil {
+		return err
+	}
+
+	loop := true
+	var wg sync.WaitGroup
+
+	fmt.Print("Synchronizing: ")
+	wg.Add(1)
+	go func() {
+		_, err = client.Users.Sync()
+		loop = false
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		for loop {
+			fmt.Print(".")
+			time.Sleep(time.Second)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	return err
+})
