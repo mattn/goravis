1f17710cec140024e3cb5ad15394d43396749f27
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-25 10:16:09 +0900

    add whatsup

---
+++ whatsup.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Ableton/go-travis"
+	"github.com/alecthomas/kingpin"
+)
+
+var whatsUpCommand = kingpin.Command("whatsup", "Lists most recent builds.").Action(func(ctx *kingpin.ParseContext) error {
+	err := auth()
+	if err != nil {
+		return err
+	}
+
+	u, resp, err := client.Users.GetAuthenticated()
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	repos, resp, err := client.Repositories.Find(&travis.RepositoryListOptions{Member: u.Name})
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+	for _, repo := range repos {
+		build, _, _, resp, err := client.Builds.Get(repo.LastBuildId)
+		if err != nil {
+			continue
+		}
+		resp.Body.Close()
+		fmt.Printf("%s %s: #%d\n", repo.Slug, build.State, build.Id)
+	}
+	return nil
+})
