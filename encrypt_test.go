a92acaef874b398d771872b740814317224e670b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-25 17:39:48 +0900

    add test

--- encrypt_test.go
+++ encrypt_test.go
@@ -10,4 +10,8 @@ func TestEncrypt(t *testing.T) {
 	if content != "bar" {
 		t.Fatalf("should be %v but %v:", "bar", content)
 	}
+	content = os.Getenv("bar")
+	if content != "baz" {
+		t.Fatalf("should be %v but %v:", "baz", content)
+	}
 }
