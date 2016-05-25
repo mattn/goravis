e078440d2f75a54e744fcd08776f52d14c18d163
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-26 01:25:04 +0900

    add open

---
+++ open.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alecthomas/kingpin"
+	"github.com/skratchdot/open-golang/open"
+)
+
+var openCommand = kingpin.Command("open", "opens a build or job in the browser").Action(func(ctx *kingpin.ParseContext) error {
+	u := fmt.Sprintf("https://github.com/%s", slug())
+	return open.Run(u)
+})
