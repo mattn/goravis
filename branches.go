22d042910927cc2dad91a80a38037aaa91e6ec50
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-05-28 20:01:00 +0900

    pull-request information

--- branches.go
+++ branches.go
@@ -18,12 +18,20 @@ var branchesCommand = kingpin.Command("branches", "displays the most recent buil
 	}
 
 	for _, branch := range branches {
-		b, _, err := client.Commits.GetFromBuild(branch.Id)
+		commit, _, err := client.Commits.GetFromBuild(branch.Id)
 		if err != nil {
 			return err
 		}
 
-		fmt.Printf("%s %s %s\n", b.Branch, branch.Number, branch.State)
+		if branch.PullRequest {
+			build, _, _, _, err := client.Builds.Get(branch.Id)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s %s %s (PR #%d)\n", commit.Branch, branch.Number, branch.State, build.PullRequestNumber)
+		} else {
+			fmt.Printf("%s %s %s\n", commit.Branch, branch.Number, branch.State)
+		}
 	}
 	return nil
 })
